bde429579d27db2749df6c26f8d460f0d6d5dfa5
brightonmboya <b.mboya@alustudent.com>
2024-11-07 11:56:47 +0300

    task 0

---
+++ messaging_app/messaging_app/settings.py
@@ -0,0 +1,27 @@
+# messaging_app/settings.py
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    
+    # Third party apps
+    'rest_framework',
+    
+    # Local apps
+    'chats',
+]
+
+# Django REST Framework settings
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}
