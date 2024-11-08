45a07729f7db4c6abb764360c0c5d5f590dac81b
brightonmboya <b.mboya@alustudent.com>
2024-11-08 13:03:55 +0300

    task 3.4

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -1,21 +1,43 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
+from mptt.models import MPTTModel, TreeForeignKey
 
-class Message(models.Model):
+class Message(MPTTModel):
     sender = models.ForeignKey(
         User, 
         related_name='sent_messages',
-        on_delete=models.CASCADE  # Will delete messages when user is deleted
+        on_delete=models.CASCADE
     )
     receiver = models.ForeignKey(
         User,
         related_name='received_messages',
-        on_delete=models.CASCADE  # Will delete messages when user is deleted
+        on_delete=models.CASCADE
     )
     content = models.TextField()
-    timestamp = models.DateTimeField(auto_now_add=True)
+    timestamp = models.DateTimeField(default=timezone.now)
     is_read = models.BooleanField(default=False)
+    parent_message = TreeForeignKey(
+        'self',
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name='replies'
+    )
+    
+    class MPTTMeta:
+        order_insertion_by = ['timestamp']
+    
+    class Meta:
+        ordering = ['tree_id', 'lft']
+    
+    def __str__(self):
+        return f"From {self.sender} to {self.receiver} at {self.timestamp}"
+    
+    @property
+    def thread_id(self):
+        """Return the ID of the root message in this thread"""
+        return self.get_root().id
     
 class MessageHistory(models.Model):
     message = models.ForeignKey(
--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -21,3 +21,58 @@ def delete_user(request):
     user.delete()
     messages.success(request, 'Your account has been successfully deleted.')
     return redirect('home')
+
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.db.models import Q, Prefetch
+from .models import Message
+from .forms import MessageForm
+
+@login_required
+def conversation_list(request):
+    """Display list of unique conversations"""
+    # Get root messages (messages without parents) for the user
+    conversations = Message.objects.root_nodes().filter(
+        Q(sender=request.user) | Q(receiver=request.user)
+    ).select_related('sender', 'receiver').order_by('-timestamp')
+    
+    return render(request, 'messaging/conversation_list.html', {
+        'conversations': conversations
+    })
+
+@login_required
+def conversation_detail(request, message_id):
+    """Display a threaded conversation"""
+    root_message = get_object_or_404(
+        Message.objects.select_related('sender', 'receiver'),
+        id=message_id
+    )
+    
+    # Get all messages in the thread
+    thread_messages = (
+        Message.objects.filter(tree_id=root_message.tree_id)
+        .select_related('sender', 'receiver')
+        .order_by('tree_id', 'lft')
+    )
+    
+    if request.method == 'POST':
+        form = MessageForm(request.POST)
+        if form.is_valid():
+            reply = form.save(commit=False)
+            reply.sender = request.user
+            reply.receiver = (
+                root_message.sender 
+                if root_message.sender != request.user 
+                else root_message.receiver
+            )
+            reply.parent = root_message
+            reply.save()
+            return redirect('messaging:conversation_detail', message_id=message_id)
+    else:
+        form = MessageForm()
+    
+    return render(request, 'messaging/conversation_detail.html', {
+        'root_message': root_message,
+        'thread_messages': thread_messages,
+        'form': form
+    })
